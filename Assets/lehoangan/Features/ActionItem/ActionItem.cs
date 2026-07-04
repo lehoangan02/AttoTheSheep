1571be9991fdbcd478c5848d4ee95dd522c0b4ad
lehoangan02 <146849112+lehoangan02@users.noreply.github.com>
2026-07-04 09:05:57 +0700

    add arts

---
+++ Assets/lehoangan/Features/ActionItem/ActionItem.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ActionItem", menuName = "Scriptable Objects/ActionItem")]
+public class ActionItem : ScriptableObject
+{
+    public string itemName;
+    public string itemDescription;
+    public Sprite icon;
+}
