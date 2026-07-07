e27c0d9df2d66f13294c8fdd36b0432ce3e960e7
Chris-KH <klowgamervn@gmail.com>
2026-07-07 15:10:35 +0700

    Inventory

--- Assets/Scripts/UI/ShopAndInventory/ShopInventoryRoot.cs
+++ Assets/Scripts/UI/ShopAndInventory/ShopInventoryRoot.cs
@@ -10,18 +10,34 @@ namespace AttoTheSheep.UI.ShopAndInventory
 
         private void Start()
         {
-            Button bagBtn = GameObject.Find("BagButton")?.GetComponent<Button>();
-            if (bagBtn != null) 
+            GameObject bagGo = GameObject.Find("BagButton");
+            if (bagGo != null)
             {
-                bagBtn.onClick.RemoveAllListeners();
-                bagBtn.onClick.AddListener(() => inventoryPanel.SetActive(true));
+                Button bagBtn = bagGo.GetComponent<Button>();
+                if (bagBtn != null) 
+                {
+                    bagBtn.onClick.RemoveAllListeners();
+                    bagBtn.onClick.AddListener(() => inventoryPanel.SetActive(true));
+                }
+            }
+            else
+            {
+                Debug.LogWarning("[ShopInventoryRoot] Không tìm thấy GameObject nào tên 'BagButton' trên Scene để móc sự kiện!");
             }
 
-            Button shopBtn = GameObject.Find("ShopButton")?.GetComponent<Button>();
-            if (shopBtn != null) 
+            GameObject shopGo = GameObject.Find("ShopButton");
+            if (shopGo != null)
             {
-                shopBtn.onClick.RemoveAllListeners();
-                shopBtn.onClick.AddListener(() => shopPanel.SetActive(true));
+                Button shopBtn = shopGo.GetComponent<Button>();
+                if (shopBtn != null) 
+                {
+                    shopBtn.onClick.RemoveAllListeners();
+                    shopBtn.onClick.AddListener(() => shopPanel.SetActive(true));
+                }
+            }
+            else
+            {
+                Debug.LogWarning("[ShopInventoryRoot] Không tìm thấy GameObject nào tên 'ShopButton' trên Scene để móc sự kiện!");
             }
 
             // Gắn sự kiện cho các nút CloseBtn bên trong Panel
