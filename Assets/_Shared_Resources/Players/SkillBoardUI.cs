4bb76649b4c7591b812675a79a15030a9d474317
lehoangan02 <146849112+lehoangan02@users.noreply.github.com>
2026-07-10 21:31:48 +0700

    Change stuff

--- Assets/_Shared_Resources/Players/SkillBoardUI.cs
+++ Assets/_Shared_Resources/Players/SkillBoardUI.cs
@@ -6,6 +6,14 @@ public class SkillBoardUI : MonoBehaviour
     [Header("UI Slots (Kéo 4 SkillUISlot từ Hierarchy vào đây)")]
     public List<SkillUIManager> uiSlots = new List<SkillUIManager>();
 
+    private void Start()
+    {
+        if (Application.isMobilePlatform)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
     // Hàm này sẽ được gọi khi Local Player spawn thành công
     public void InitializeSkillBoard(PlayerSkills localPlayerSkills)
     {
